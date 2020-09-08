f9400aea7ba06131ebf05f52a9769a5a2bbe5e54
Estevanbs <estevanbartmann@gmail.com>
2020-09-08 08:25:21 -0300

    Primeiro commiit

---
+++ src/components/Lista.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="container">
+      <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">ID</th>
+            <th scope="col">Nome</th>
+            <th scope="col">Lotes</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="lista in lista" :key="lista.id">
+            <td>{{ lista.id }}</td>
+            <td>{{ lista.nome }}</td>
+            <td>{{ lista.lotes }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  
+  name: "Lista",
+
+  data() {
+    return {
+      lista: [],
+    };
+  },
+  created() {
+      axios
+        .get("https://api.kleiloes.com.br/tipo-bem/ativos")
+        .then((response) => {
+          this.lista = response.data;
+        });
+    },
+  methods: {
+    
+  },
+};
+</script>
